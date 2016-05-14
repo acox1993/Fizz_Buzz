91eac086b9021f310361d15d264e9b913d0b7bfc
acox1993 <alexandriacrmu@gmail.com>
2016-05-14 10:03:30 -0400

    update

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-app.js
+$(document).ready(function(){
+	for (var i = 1; i < 100; i++) {
+   // console.log(i);
+   // $('body').append(i);
+   if (i % 3 == 0 && i % 5 == 0){
+   		$('body').append("Fizz Buzz, ")
+   } 
+   else if (i % 3 == 0) {
+   		$('body').append("Fizz, ")
+   }
+   else if (i % 5 == 0) {
+   		$('body').append("Buzz, ")
+   }
+   else {
+   		$('body').append(i).append(", ")
+   }
+}
+
+});
