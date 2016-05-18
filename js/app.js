685cef9a70c29063ecd01e9a93f66c1a27db6f8b
acox1993 <alexandriacrmu@gmail.com>
2016-05-17 23:58:08 -0400

    refract 1

--- js/app.js
+++ js/app.js
@@ -1,19 +1,41 @@
 $(document).ready(function(){
-	for (var i = 1; i <= 100; i++) {
+   function userCount() {
+      input = $('#count').val();
+      if (isNaN(input)) {
+         $('h3#head').html('Please enter a number');
+         return
+      }
+
+      else if (input < 1 || input > 100) {
+         $('h3#head').html('Please enter a number between 1 and 100');
+         return
+      };
+      $('input#count').val('').focus();
+
+      for (var i = 1; i <= input; i++) {
    // console.log(i);
    // $('body').append(i);
+
    if (i % 3 == 0 && i % 5 == 0){
-   		$('.numbers').append("<p>").append("Fizz Buzz, ").append("</p>")
+         $('.numbers').append("<p>").append("Fizz Buzz, ").append("</p>")
    } 
    else if (i % 3 == 0) {
-   		$('.numbers').append("<p>").append("Fizz, ").append("</p>")
+         $('.numbers').append("<p>").append("Fizz, ").append("</p>")
    }
    else if (i % 5 == 0) {
-   		$('.numbers').append("<p>").append("Buzz, ").append("</p>")
+         $('.numbers').append("<p>").append("Buzz, ").append("</p>")
    }
    else {
-   		$('.numbers').append("<p>").append(i).append(", ").append("</p>")
+         $('.numbers').append("<p>").append(i).append(", ").append("</p>")
    }
-}
+};
+
+   };
+
+
+
+$('#countButton').click(function() {
+      userCount();
+   });
 
 });
